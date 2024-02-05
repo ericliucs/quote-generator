969fcb6be48d219ae7e601ffa5459858f100561d
Eric Liu <liue@duq.edu>
2024-02-05 10:59:44 -0500

    Tweaked HTML IDs and classes

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
+const quote = document.getElementById("quote");
+const quoteAuthor = document.getElementById("author");
+const twitterButton = document.getElementById("twitter-button");
 
+twitterButton.addEventListener("click", (event) => {
+  const currentQuote = quote.innerText;
+  const currentQuoteAuthor = quoteAuthor.innerText;
+  console.log(currentQuote);
+  console.log(currentQuoteAuthor);
+});
